618b85fab732029dbf6ff06769839eb741b02ca8
Ann Kim <annkim822@gmail.com>
2018-06-20 13:58:59 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,5 +7,13 @@ def array_with_two_elements
 end
 
 def first_element(new_array)
-  new_array = [1]
+  new_array = [0]
+end
+
+def third_element(new_array)
+  new_array = [2]
+end
+
+def last_element(new_array)
+  new_array = [-1]
 end
